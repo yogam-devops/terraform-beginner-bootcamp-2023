33710a52aa1f7eade4cce77f0077938a3307d68e
yoga murugesan <yv270415@gmail.com>
2023-09-29 21:52:24 +0000

    #13 Created sample bucket with custom name random name had issues modified to complete the exercise

--- main.tf
+++ main.tf
@@ -4,7 +4,17 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+
   }
+}
+
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -13,12 +23,16 @@ provider "random" {
 
 
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
-  override_special = ""
+  length           = "16"
+  special          = "false"
+  lower            = "true"
+  upper            = "false"
 }
 
-output "random_bucket_name"{
-  value = random_string.bucket_name.result
+resource "aws_s3_bucket" "example" {
+  bucket = "terraformbeginnerbootcamp2023yoga"
+}
 
+output "aws_s3_bucket_bucket_name"{
+  value = aws_s3_bucket.example.arn
 }
